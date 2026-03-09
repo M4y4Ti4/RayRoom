076cd17006f75f17d7663af7e3e733f5239bd6b9
M4y4Ti4 <mayakanani@hotmail.com>
2026-03-09 13:01:43 +0100

    plotting TF and RIR

--- rayroom/core/data_anal.py
+++ rayroom/core/data_anal.py
@@ -1,7 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
 def plot_transfer_function(rir_total, fs):
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import matplotlib
+
     n = len(rir_total)
 
     H = np.fft.rfft(rir_total, n=n)
@@ -10,7 +12,7 @@ def plot_transfer_function(rir_total, fs):
     magnitude_db = 20 * np.log10(np.abs(H) + 1e-12)
 
     fig, ax = plt.subplots(figsize=(12, 5))
-    ax.plot(freqs, magnitude_db)
+    ax.semilogx(freqs, magnitude_db)
     ax.set_xlabel("Frequency (Hz)")
     ax.set_ylabel("Magnitude (dB)")
     ax.set_title("Transfer Function")
@@ -20,3 +22,15 @@ def plot_transfer_function(rir_total, fs):
     ax.set_xticks([63, 125, 250, 500, 1000, 2000, 4000])
     ax.xaxis.set_major_formatter(matplotlib.ticker.FuncFormatter(lambda x, _: str(int(x))))
     plt.show()
+
+
+def plot_rir(rir_total, fs):
+    t = np.linspace(0, 2.0, len(rir_total))
+    fig, ax = plt.subplots(figsize=(12,5))
+    ax.plot(t, rir_total)
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Amplitude")
+    ax.set_title("Impulse Response")
+    ax.grid(True, which='both', alpha=0.3)
+    plt.show()
+
